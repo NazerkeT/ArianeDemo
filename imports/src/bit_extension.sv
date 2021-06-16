669651fc35778c0bbe89913a7019a6344d630eab
Nazerke Turtayeva <nazerke.turtayeva@nu.edu.kz>
2021-06-16 13:33:46 +0600

    Update bit_extension.sv

--- imports/src/bit_extension.sv
+++ imports/src/bit_extension.sv
@@ -1,19 +1,3 @@
-// Copyright 2018 ETH Zurich and University of Bologna.
-// Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the "License"); you may not use this file except in
-// compliance with the License.  You may obtain a copy of the License at
-// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
-// or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
-//
-// Author: Matthias Baer <baermatt@student.ethz.ch>
-// Author: Igor Loi <igor.loi@unibo.it>
-// Author: Andreas Traber <atraber@student.ethz.ch>
-// Author: Lukas Mueller <lukasmue@student.ethz.ch>
-// Author: Florian Zaruba <zaruabf@iis.ee.ethz.ch>
-//
 // Date: 12.08.2020
 // Description: Seperate bit extension module to keep ALU clean
 
